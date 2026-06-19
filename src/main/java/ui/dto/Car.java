5ecce244b6979f74d1e9ab059c8456e6039ebc8a
Akhunov Gayaz <kapananas16@gmail.com>
2026-06-19 22:22:25 +0300

    Refactor Radio wrapper package and ensure newline at end of files

--- src/main/java/ui/dto/Car.java
+++ src/main/java/ui/dto/Car.java
@@ -1,6 +1,26 @@
 package ui.dto;
 
+import com.github.javafaker.Faker;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
 public class Car {
 
+    private static final Faker faker = new Faker();
+    private String[] engineTypes = {"CNG", "Diesel", "Gasoline", "Hydrogenic","PHEV"};
 
+    @Builder.Default
+    private final String engineType = faker.options().option(engineTypes);
+    //private final String engineType = faker.options().option("CNG", "Diesel", "Gasoline", "Hydrogenic","PHEV");
+    @Builder.Default
+    private final String mark = faker.company().name();
+    @Builder.Default
+    private final String model = faker.harryPotter().character();
+    @Builder.Default
+    private final BigDecimal price = BigDecimal.valueOf(faker.number().randomDouble
+            (2, 0, 999999999));
 }
