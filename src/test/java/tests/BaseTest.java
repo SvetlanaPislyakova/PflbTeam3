6c514964ffefb1994ce3f1a565440c25afe2566e
Svetlana Pislyakova <pislakovasvetlana@gmail.com>
2026-06-20 14:58:12 +0300

    maven.yml

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -60,7 +60,7 @@ public class BaseTest {
         Configuration.baseUrl = "http://82.142.167.37:4881/";
         Configuration.clickViaJs = true;
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless=new");
+        options.addArguments("--headless=new");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
