7d1600af47058da259007b3bfbbe4c6f22392147
Dmitry <Shapovalov.D.97@yandex.ru>
2026-06-22 18:39:33 +0300

    CAR

---
+++ src/test/java/tests/api/CarNegativeTestParam.java
@@ -0,0 +1,40 @@
+package tests.api;
+
+import api.adapters.CarAdapter;
+import api.models.CarRq;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.math.BigDecimal;
+
+public class CarNegativeTestParam extends BaseApiTest  {
+
+    private final CarAdapter carAdapter = new CarAdapter();
+
+    @ParameterizedTest
+    @CsvSource({
+            ",X5,Diesel,10000",
+            "BMW,,Diesel,10000",
+            "BMW,X5,,10000",
+            "BMW,X5,Diesel,",
+            "BMW,X5,Diesel,-100",
+            "BMW,X5,Diesel,0",
+            "'',X5,Diesel,10000",
+            "BMW,'',Diesel,10000"
+    })
+    void createInvalidCarTest(
+            String mark,
+            String model,
+            String engineType,
+            BigDecimal price) {
+
+        CarRq car = CarRq.builder()
+                .mark(mark)
+                .model(model)
+                .engineType(engineType)
+                .price(price)
+                .build();
+
+        carAdapter.createCarBadRequest(car, accessToken);
+    }
+}
