15a473e8d37079f78efd590564bd63739a398e4b
OlgaBaidalina <baydalina.1987@gmail.com>
2026-06-28 21:58:12 +0400

    Добавила оставшиеся UI-тесты All POST и доработала wrappers Radio/Table

--- src/main/java/ui/pages/AllPostPage.java
+++ src/main/java/ui/pages/AllPostPage.java
@@ -2,6 +2,8 @@ package ui.pages;
 
 import com.codeborne.selenide.SelenideElement;
 import ui.dto.Car;
+import ui.dto.User;
+import ui.wrappers.Radio;
 import ui.wrappers.Table;
 
 import java.math.BigDecimal;
@@ -15,6 +17,11 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.sleep;
 
 public class AllPostPage extends BasePage {
+
+    private static final String BUY_OR_SELL_CAR_FORM =
+            "//*[th[contains(text(), 'User ID')] and th[contains(text(), 'Car Id')]]/ancestor::table";
+    private static final String SETTLE_OR_EVICT_FORM =
+            "//*[th[contains(text(), 'User ID')] and th[contains(text(), 'House ID')]]/ancestor::table";
 
     private final Table createUserTable = new Table("Create new user");
     private final Table addMoneyTable = new Table("Add money");
@@ -70,13 +77,102 @@ public class AllPostPage extends BasePage {
         return this;
     }
 
+    public AllPostPage createUser(User user) {
+        createUserTable.setValueToInput("First Name", user.getFirstName());
+        createUserTable.setValueToInput("Last Name", user.getLastName());
+        createUserTable.setValueToInput("Age", String.valueOf(user.getAge()));
+        if (user.getSex() != null) {
+            Radio.byNameAndValue("sex_send", user.getSex()).select();
+        }
+        createUserTable.setValueToInput("Money", String.valueOf(user.getMoney()));
+        createUserTable.clickPushToApiBtn();
+        return this;
+    }
+
+    public String getCreateUserStatusMessage() {
+        return createUserTable.getMessagePushToApi();
+    }
+
+    public Integer getCreateUserStatusCode() {
+        return createUserTable.getStatus();
+    }
+
+    public Integer getCreateUserId() {
+        return createUserTable.getResultInt();
+    }
+
+    public AllPostPage addMoneyToUser(Integer userId, BigDecimal amount) {
+        addMoneyTable.setValueToInput("User ID", String.valueOf(userId));
+        addMoneyTable.setValueToInput("Money", String.valueOf(amount));
+        addMoneyTable.clickPushToApiBtn();
+        return this;
+    }
+
+    public String getAddMoneyStatusMessage() {
+        return addMoneyTable.getMessagePushToApi();
+    }
+
+    public Integer getAddMoneyStatusCode() {
+        return addMoneyTable.getStatus();
+    }
+
+    public Double getAddMoneyResult() {
+        return addMoneyTable.getResultDouble();
+    }
+
+    public AllPostPage buyCarForUser(Integer userId, Integer carId) {
+        buyOrSellCarTable.setValueToInput("User ID", String.valueOf(userId));
+        buyOrSellCarTable.setValueToInput("Car Id", String.valueOf(carId));
+        Radio.byValueIn(BUY_OR_SELL_CAR_FORM, "buyCar").select();
+        buyOrSellCarTable.clickPushToApiBtn();
+        return this;
+    }
+
+    public AllPostPage sellCarForUser(Integer userId, Integer carId) {
+        buyOrSellCarTable.setValueToInput("User ID", String.valueOf(userId));
+        buyOrSellCarTable.setValueToInput("Car Id", String.valueOf(carId));
+        Radio.byValueIn(BUY_OR_SELL_CAR_FORM, "sellCar").select();
+        buyOrSellCarTable.clickPushToApiBtn();
+        return this;
+    }
+
+    public String getBuyOrSellCarStatusMessage() {
+        return buyOrSellCarTable.getMessagePushToApi();
+    }
+
+    public Integer getBuyOrSellCarStatusCode() {
+        return buyOrSellCarTable.getStatus();
+    }
+
+    public AllPostPage settleUserToHouse(Integer userId, Integer houseId) {
+        settleOrEvictUserTable.setValueToInput("User ID", String.valueOf(userId));
+        settleOrEvictUserTable.setValueToInput("House ID", String.valueOf(houseId));
+        Radio.byValueIn(SETTLE_OR_EVICT_FORM, "settle").select();
+        settleOrEvictUserTable.clickPushToApiBtn();
+        return this;
+    }
+
+    public AllPostPage evictUserFromHouse(Integer userId, Integer houseId) {
+        settleOrEvictUserTable.setValueToInput("User ID", String.valueOf(userId));
+        settleOrEvictUserTable.setValueToInput("House ID", String.valueOf(houseId));
+        Radio.byValueIn(SETTLE_OR_EVICT_FORM, "evict").select();
+        settleOrEvictUserTable.clickPushToApiBtn();
+        return this;
+    }
+
+    public String getSettleOrEvictStatusMessage() {
+        return settleOrEvictUserTable.getMessagePushToApi();
+    }
+
+    public Integer getSettleOrEvictStatusCode() {
+        return settleOrEvictUserTable.getStatus();
+    }
+
     public AllPostPage createCar(Car car) {
-        setInput("#car_mark_send", car.getMark());
-        setInput("#car_model_send", car.getModel());
-        setInput("#car_price_send", String.valueOf(car.getPrice()));
-        setInput("#car_engine_type_send", car.getEngineType());
-        // Форма All POST сбрасывает Mark после изменения Engine Type; повторно выставляем его перед submit.
-        setInput("#car_mark_send", car.getMark());
+        createCarTable.setValueToInput("Engine Type", car.getEngineType());
+        createCarTable.setValueToInput("Mark", car.getMark());
+        createCarTable.setValueToInput("Model", car.getModel());
+        createCarTable.setValueToInput("Price", String.valueOf(car.getPrice()));
         createCarTable.clickPushToApiBtn();
         return this;
     }
@@ -94,14 +190,8 @@ public class AllPostPage extends BasePage {
     }
 
     public AllPostPage createHouse(Integer floors, BigDecimal price) {
-        for (int i = 0; i < 3; i++) {
-            fillHouseForm(floors, price);
-            createHouseTable.clickPushToApiBtn();
-            if (!getCreateHouseStatusMessage().contains("Invalid input data")) {
-                return this;
-            }
-            openPage().isPageOpened();
-        }
+        fillHouseForm(floors, price);
+        createHouseTable.clickPushToApiBtn();
         return this;
     }
 
@@ -112,14 +202,6 @@ public class AllPostPage extends BasePage {
         setHouseInput("#parking_second_send", "0");
         setHouseInput("#parking_third_send", "0");
         setHouseInput("#parking_fourth_send", "0");
-    }
-
-    private void setInput(String selector, String fieldValue) {
-        SelenideElement input = $(selector).shouldBe(visible).shouldBe(enabled);
-        input.click();
-        input.setValue(fieldValue);
-        input.shouldHave(value(fieldValue));
-        sleep(200);
     }
 
     private void setHouseInput(String selector, String fieldValue) {
