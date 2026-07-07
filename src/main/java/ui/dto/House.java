51b2493cd597a899d3d5f8cab583446b6da9b6a4
Olga Baydalina <baydalina.1987@gmail.com>
2026-07-07 05:38:20 +0400

    Add All POST API and UI tests

--- src/main/java/ui/dto/House.java
+++ src/main/java/ui/dto/House.java
@@ -1,4 +1,17 @@
 package ui.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder(toBuilder = true)
 public class House {
+    private final Integer floors;
+    private final BigDecimal price;
+    private final Integer warmCoveredParking;
+    private final Integer warmNotCoveredParking;
+    private final Integer coldCoveredParking;
+    private final Integer coldNotCoveredParking;
 }
